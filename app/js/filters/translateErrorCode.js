57072bcd59cfb19be5cd0e643227f173c9a36821
marushkevych <marushkevych@gmail.com>
2015-03-31 17:32:10 -0400

    removing gettext dependency

--- app/js/filters/translateErrorCode.js
+++ app/js/filters/translateErrorCode.js
@@ -4,33 +4,16 @@ module.exports = translateErrorCode;
 
 var ErrorCodes = {
     
-    BAD_REQUEST: {
-        en: "Unfortunately, we're unable to process your request. We apologize for the inconvenience.",
-        fr: "Malheureusement, il nous est impossible de donner suite à votre demande. Nous sommes désolés des inconvénients subis, le cas échéant."
-    },
+    BAD_REQUEST: "Unfortunately, we're unable to process your request. We apologize for the inconvenience.",
     
-    DUPLICATE_ITEM: {
-        en: "Can not add duplicate item! Plese try again.",
-        fr: "Vous ne pouvez pas ajouter l'article en double ! Plese réessayer ."
-    },
+    DUPLICATE_ITEM: "Can not add duplicate item! Plese try again.",
     
-    SESSION_EXPIRED: {
-        en: "Your session has expired.",
-        fr: "Votre session a expiré."        
-    }
+    SESSION_EXPIRED: "Your session has expired.",
 };
 
 
-translateErrorCode.$inject = ['gettextCatalog'];
 
-function translateErrorCode (gettextCatalog) {
-    return function(errorCode) {
-        var lang = gettextCatalog.currentLanguage === "fr_CA" ? 'fr' : 'en';
-        if(ErrorCodes[errorCode] && ErrorCodes[errorCode][lang]){
-            return ErrorCodes[errorCode][lang];
-        }else{
-            return errorCode;
-        }
-    };
+function translateErrorCode () {
+    return ErrorCodes[errorCode] || errorCode;
+};
     
-}
